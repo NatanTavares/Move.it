7ae404a9df8b5e4adc7a65c5e83f61e67cbdf243
Natan <nattantavares.s15@gmail.com>
2021-02-24 15:59:20 -0300

    Implemented the random challenge generator functionality

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,24 +1,28 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const hasActiveChallenge = true;
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
 
   return (
     <div className={styles.challengeBoxContainer}>
-      {hasActiveChallenge ? (
+      {activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400 xp</header>
+          <header>Ganhe {activeChallenge.amount} xp</header>
 
           <main>
-            <img src="icons/body.svg" />
+            <img src={`icons/${activeChallenge.type}.svg`} />
             <strong>Novo desafio</strong>
-            <p>Levante e faça uma caminha de 3 minutos.</p>
+            <p>{activeChallenge.description}</p>
           </main>
           
           <footer>
             <button 
               type="button"
               className={styles.challengeFailedButton}
+              onClick={resetChallenge}
             >
               Falhei
             </button>
@@ -26,8 +30,8 @@ export function ChallengeBox() {
               type="button"
               className={styles.challengeSucceededButton}
             >
-              Comple
-            tei</button>
+              Completei
+            </button>
           </footer>
         </div>
       ) : (
